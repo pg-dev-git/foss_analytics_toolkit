5865c264f50a91181934d2240831a5c3a1818b87
pg-dev-git <pedro.gagliardi.sfdc@gmail.com>
2021-05-06 16:26:37 -0400

    updated version

--- setup_mac.py
+++ setup_mac.py
@@ -8,8 +8,8 @@ executables = [
 
 setup(
     name="tcrm_toolkit",
-    version="0.1b5",
-    description="TCRM Toolkit v0.1-beta5",
+    version="0.1b6",
+    description="TCRM Toolkit v0.1-beta6",
     executables=executables,
     options=options,
 )
--- sfdc_login.py
+++ sfdc_login.py
@@ -226,7 +226,7 @@ class sfdc_login():
         time.sleep(0.5)
         tprint("     Tool Kit     ")
         time.sleep(0.5)
-        prGreen("                                                                       v0.1-beta5" + "\r\n")
+        prGreen("                                                                       v0.1-beta6" + "\r\n")
         prCyan("                                                             Salesforce API v51.0")
         print("\r\n")
         time.sleep(0.5)
