afa69cb25cd1df4d328519e8cce7c167ab8b99fe
Pedro Gagliardi <82517919+pg-dev-git@users.noreply.github.com>
2022-03-17 13:53:02 -0400

    Delete pd_combine_csv.py

--- pd_combine_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import glob
-import os
-
-extension = 'csv'
-csv_files = glob.glob('*.{}'.format(extension))
-combined_csv = pd.concat([pd.read_csv(csv_file, low_memory=False) for csv_file in csv_files])
-combined_csv.to_csv( "AcquiredAccount_combined_csv.csv", index=False, encoding='utf-8-sig')
